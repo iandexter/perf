75d002c76b5b06143a343e0294d71ae744a8d17b
Ian Dexter R Marquez <iandexter+tech@gmail.com>
2013-11-05 16:07:01 +0800

    code rework - Break down into functions.

--- getweeklysar.sh
+++ getweeklysar.sh
@@ -2,10 +2,25 @@
 #
 # Gather weekly sar report.
 
+### vars
+h=$(hostname -f)
+
 start_date=$(date --date="last week" +%Y%m%d)
 end_date=$(date --date="yesterday" +%Y%m%d)
 
-sar_output_path=/var/tmp/
+sar_types=( b d n r u )
+
+sar_output_path=/var/tmp
+sar_output_file="${h}-sar_${sar_types}_${start_date}-${end_date}.dat"
+sar_output_file="${sar_output_path}/${sar_output_file}"
+
+### functions
+cleanup() {
+    if [[ -e "${sar_output_file}" ]] ; then
+        ( echo "[I] Found old files. Cleaning up first."; \
+          rm -f ${sar_output_path}/*_${start_date}-${end_date}.dat )
+    fi
+}
 
 getsar() {
     sar_type=$1
@@ -13,42 +28,64 @@ getsar() {
     month=$3
     day=$4
 
-    sar_output_file=$(hostname)-sar_${sar_type}_${start_date}-${end_date}.dat
+    sar_output_file=${h}-sar_${sar_type}_${start_date}-${end_date}.dat
+    sar_output_file=${sar_output_path}/${sar_output_file}
 
     if [[ "${sar_type}" == "n" ]] ; then
-        eth=$(ifconfig | awk '/eth/ {print $1}')
-        LC_ALL=C sar -n DEV -f ${sar_file} | grep ^[0-9] | \
+        eth=$(/sbin/ifconfig | awk '/eth/ {print $1}')
+        LC_ALL=C sar -n DEV -f ${sar_file} 2>/dev/null | grep ^[0-9] | \
             grep -E "${eth}|IFACE" | \
             sed "s/^/${month}-${day}\ /" | \
-            tee -a ${sar_output_path}/${sar_output_file} &>/dev/null
+            tee -a ${sar_output_file} &>/dev/null
     else
         [[ "${sar_type}" == "d" ]] && sar_type="d -p"
-        LC_ALL=C sar -${sar_type} -f ${sar_file} | grep ^[0-9] | \
+        LC_ALL=C sar -${sar_type} -f ${sar_file} 2>/dev/null | grep ^[0-9] | \
             sed -s "s/^/${month}-${day}\ /" | \
-            tee -a ${sar_output_path}/${sar_output_file} &>/dev/null
+            tee -a ${sar_output_file} &>/dev/null
     fi
 }
 
-for f in $(seq 6 -1 1) ; do
-    sar_file=$(find /var/log/sa -maxdepth 1 -mtime $f -iname 'sa*' | \
-        grep -v sar)
-    month=$(date --date="${f} days ago" +%b)
-    day=$(date --date="$((f+1)) days ago" +%d)
-    echo -n "Getting sar file from ${month} ${day}... "
-    for sar_type in u n b d ; do
+getsartype() {
+    sar_file=$1
+    month=$2
+    day=$3
+
+    echo -n "${month} ${day}"
+    for sar_type in "${sar_types[@]}" ; do
         getsar ${sar_type} ${sar_file} ${month} ${day}
     done
-    echo "done."
-done
+}
 
-month=$(date --date="yesterday" +%b)
-yesterday=$(date --date="yesterday" +%d)
-sar_file=/var/log/sa/sa${yesterday}
-echo -n "Getting sar file from ${month} ${yesterday}... "
-for sar_type in u n b d ; do
-    getsar ${sar_type} ${sar_file} ${month} ${yesterday}
-done
-echo "done."
+getlastweek() {
+    echo -n "Getting sar files from "
+    for f in $(seq 6 -1 1) ; do
+        sar_file=$(find /var/log/sa -maxdepth 1 -mtime $f -iname 'sa*' | \
+                   grep -v sar)
+        month=$(date --date="${f} days ago" +%b)
+        day=$(date --date="$((f+1)) days ago" +%d)
+        getsartype ${sar_file} ${month} ${day}
+        echo -n ", "
+    done
+}
 
-echo "SAR data files:"
-ls -1 /var/tmp/*sar*${start_date}-${end_date}.dat
+getyesterday() {
+    month=$(date --date="yesterday" +%b)
+    day=$(date --date="yesterday" +%d)
+    sar_file=/var/log/sa/sa${day}
+    getsartype ${sar_file} ${month} ${day}
+    echo " - done"
+}
+
+listfiles() {
+    echo "SAR data files:"
+    ls -1 /var/tmp/*sar*${start_date}-${end_date}.dat
+}
+
+### main()
+unalias rm &>/dev/null
+umask 022 &>/dev/null
+
+cleanup
+getlastweek
+getyesterday
+listfiles
